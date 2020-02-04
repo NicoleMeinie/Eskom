9f351b08989b5c2dc7d787481f1e0159392ec474
NicoleMeinie <60350495+NicoleMeinie@users.noreply.github.com>
2020-02-04 16:09:16 +0200

    added q1 and q3 calculations

--- function2.py
+++ function2.py
@@ -1,29 +1,38 @@
 def five_number_summary(lists):
 
-"""This function takes in a list of integers and returns a dictionary of the five number summary. 
-   Answers are rounded to the nearest second decimal.
-"""   
-  lists.sort()
+    lists.sort()
 
-  minimum = round(min(lists),2)
-  maximum = round(max(lists),2)
-  mean = round(sum(lists)/len(lists),2)
+    minimum = round(min(lists),2)
+    maximum = round(max(lists),2)
+    mean = round(sum(lists)/len(lists),2)
 
-  if len(lists) % 2 == 0: 
+    if len(lists) % 2 == 0: 
 
-    p1 = int((len(lists)/2))
-    p2 = int((len(lists)/2)-1)
-    median = round((lists[p1] + lists[p2])/2,2)
+      p1 = int((len(lists)/2))
+      p2 = int((len(lists)/2)-1)
+      median = round((lists[p1] + lists[p2])/2,2)
 
-    q1 = round((lists[(len(lists)+1)//4] + lists[(len(lists)+1)//4 + 1])/2,2)
+      top_half = lists[:len(lists)//2]
+      bottom_half = lists[len(lists)//2:]
 
-    q3 = round((lists[3*(len(lists)+1)//4] + lists[(3*(len(lists)+1)//4)) + 1])/2,2)
+      print(bottom_half)
+      print(top_half)
 
-   else:
-     
-    median = round(lists[len(lists)//2],2)
+      q1 = (top_half[len(top_half)//2] + top_half[(len(top_half)//2) -1])/2
+      q3 = (bottom_half[len(bottom_half)//2] + bottom_half[(len(bottom_half)//2) -1 ])/2
 
-    q1 = 
+    else:
+      median = round(lists[len(lists)//2],2)
+    
+      top_half = lists[:len(lists)//2+1]
+      bottom_half = lists[(len(lists)//2)+1:]
+      
+      if top_half % 2 == 0:
+        q1 = round((top_half[len(lists)//2] + top_half[(len(lists)//2) - 1])/2,2)
+        q3 = round((bottom_half[len(lists)//2] + bottom_half[(len(lists)//2)-1])/2,2)
+        
+      else:
+        q1 = round(top_half[len(lists)//2],2)
+        q3 = round(bottom_half[len(lists)//2],2)
 
-
-    return {'max': maximum, 'q2': q2, 'median': median, 'q3': q3, 'min': minumum} 
+    return {'max': maximum, 'q1': q1, 'median': median, 'q3': q3, 'min': minimum} 
