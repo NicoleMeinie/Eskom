8fe25e6fe69bcfccc1bc5bbf28400877062013d0
NicoleMeinie <60350495+NicoleMeinie@users.noreply.github.com>
2020-02-05 12:33:40 +0200

    Created function6.py

---
+++ Function6.py
@@ -0,0 +1,4 @@
+def word_splitter(df):
+  """Write a function which splits a sentence into a list of the separate words"""
+
+return df['split tweets'] = [string.split().lowercase() for string in df['Tweets']]
